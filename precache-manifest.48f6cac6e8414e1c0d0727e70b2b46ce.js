8437c8ddd28d4def386e52b1c36b1036ff2db8ff
amyshchyshen <korym85@gmail.com>
2020-02-05 23:57:25 +0200

    Updates

---
+++ precache-manifest.48f6cac6e8414e1c0d0727e70b2b46ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "488571c659a0210a806a8fd6c1c3167e",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "e9fa841e2b0f020c2a94",
+    "url": "/goit-react-hw-03-image-finder/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "7c7c771bb1359783b20b",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.b20f4823.chunk.css"
+  },
+  {
+    "revision": "e9fa841e2b0f020c2a94",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.5a71380a.chunk.js"
+  },
+  {
+    "revision": "7c7c771bb1359783b20b",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.8df80958.chunk.js"
+  },
+  {
+    "revision": "39e1ac4a98b671bd81d0",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.1802c7e4.js"
+  }
+]);
